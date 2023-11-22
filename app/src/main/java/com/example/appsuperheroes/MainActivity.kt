cc8589a1cc879f6c9584198a9db1eb9aa4f14e27
wendellchabaud <wendelledgardossantoschabaud@gmail.com>
2023-11-22 13:53:47 +0100

    Agregada LazyColumn con los elementos de los supherHeroes

--- app/src/main/java/com/example/appsuperheroes/MainActivity.kt
+++ app/src/main/java/com/example/appsuperheroes/MainActivity.kt
@@ -6,19 +6,32 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appsuperheroes.data.SuperHeroesRepository
+import com.example.appsuperheroes.model.Superheroe
 import com.example.appsuperheroes.ui.theme.AppSuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,13 +54,11 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppSuperheroes(modifier: Modifier = Modifier) {
-
     Scaffold(
         modifier = modifier
     ) { paddingValues ->
         contenido(paddingValues = paddingValues)
     }
-
 }
 
 @Composable
@@ -55,29 +66,60 @@ fun contenido(
     paddingValues: PaddingValues,
     modifier: Modifier = Modifier
 ) {
-
     LazyColumn(
         content = {
-
+            items(SuperHeroesRepository.superHeroesList) {
+                superHeroeItem(
+                    heroe = it,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                )
+            }
         },
-        modifier = modifier
+        modifier = modifier.fillMaxSize()
     )
-
 }
 
 @Composable
-fun tarxetaSuperheroe() {
-
-
-
-}
-
-@Composable
-fun imaxe(
-    @DrawableRes imaxeId: Int,
+fun superHeroeItem(
+    heroe: Superheroe,
     modifier: Modifier = Modifier
 ) {
 
+   Card(
+       modifier = modifier.fillMaxWidth()
+   ) {
+       textoHeroe(
+           texto = heroe.nome,
+           tamanhoLetra = 15
+       )
+       imaxeHeroe(
+           heroe.imaxeId,
+           modifier = Modifier
+               .align(alignment = Alignment.End)
+               .size(100.dp)
+       )
+
+       Spacer(modifier = Modifier.height(10.dp))
+
+       textoHeroe(
+           texto = R.string.extra
+       )
+
+       textoHeroe(
+           texto = heroe.about
+       )
+   }
+
+
+}
+
+@Composable
+fun imaxeHeroe(
+    @DrawableRes imaxeId: Int,
+    modifier: Modifier = Modifier
+) {
     Image(
         painterResource(id = imaxeId),
         contentDescription = null,
@@ -87,15 +129,17 @@ fun imaxe(
 }
 
 @Composable
-fun texto(
+fun textoHeroe(
     @StringRes texto: Int,
+    tamanhoLetra: Int = 10,
     modifier: Modifier = Modifier
 ) {
 
     Text(
         text = stringResource(id = texto),
-        modifier = modifier
-    )
+        fontSize = tamanhoLetra.sp,
+        modifier = modifier,
+        )
 
 }
 
--- app/src/main/java/com/example/appsuperheroes/data/SuperHeroesRepository.kt
+++ app/src/main/java/com/example/appsuperheroes/data/SuperHeroesRepository.kt
@@ -5,6 +5,11 @@ import com.example.appsuperheroes.model.Superheroe
 
 object SuperHeroesRepository {
     val superHeroesList = listOf<Superheroe>(
-        Superheroe(imaxeId = R.drawable.android_superhero1, nome = R.string.hero1, about = R.string.description1)
+        Superheroe(imaxeId = R.drawable.android_superhero1, nome = R.string.hero1, about = R.string.description1),
+        Superheroe(imaxeId = R.drawable.android_superhero2,nome=R.string.hero2,about=R.string.description2),
+        Superheroe(imaxeId = R.drawable.android_superhero3,nome=R.string.hero3,about=R.string.description3),
+        Superheroe(imaxeId = R.drawable.android_superhero4,nome=R.string.hero4,about=R.string.description4),
+        Superheroe(imaxeId = R.drawable.android_superhero5,nome=R.string.hero5,about=R.string.description5),
+        Superheroe(imaxeId = R.drawable.android_superhero6,nome=R.string.hero6,about=R.string.description6)
     )
 }
