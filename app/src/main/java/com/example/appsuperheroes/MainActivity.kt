495108bfe65b21ff248c6b909bc302a330f30ec0
wendelledgardossantoschabaud <wendelledgardossantoschabaud@gmail.com>
2023-11-23 19:39:00 +0100

    agregado efecto al desplegar

--- app/src/main/java/com/example/appsuperheroes/MainActivity.kt
+++ app/src/main/java/com/example/appsuperheroes/MainActivity.kt
@@ -5,33 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -41,23 +37,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.appsuperheroes.data.SuperHeroesRepository
@@ -101,34 +90,42 @@ fun contenido(
             .fillMaxSize()
     ) {
         items(SuperHeroesRepository.superHeroesList) {
-            SuperHeroeCard(heroe = it)
+            SuperHeroeCard(
+                heroe = it
+            )
         }
     }
 }
 
 @Composable
-fun SuperHeroeCard(heroe: Superheroe) {
+fun SuperHeroeCard(
+    heroe: Superheroe,
+    modifier: Modifier = Modifier
+) {
 
     var expanded by remember { mutableStateOf(false) }
 
-    val caracterArriba: String = "˄"
-    val caracterAbajo: String = "˅"
-
-    var iconoBoton  = caracterAbajo
-
-    if(expanded) iconoBoton = caracterArriba ?: caracterAbajo
+    val cardBackgroundColor by animateColorAsState(
+        targetValue = if(expanded) MaterialTheme.colorScheme.tertiaryContainer else
+            MaterialTheme.colorScheme.primaryContainer, label = ""
+    )
 
     Card(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .padding(8.dp)
-            .background(color = if(expanded) Color.Magenta else Color.Cyan)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioLowBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+            .background(color = cardBackgroundColor)
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
-
         ) {
             Column(
                 modifier = Modifier
@@ -140,30 +137,36 @@ fun SuperHeroeCard(heroe: Superheroe) {
                     description = heroe.description
                 )
             }
-
-            Button(
-                colors = ButtonDefaults.buttonColors(Color.Transparent),
-                onClick = { expanded = !expanded },
-                enabled = true
-            ) {
-
-            Text(
-                text = iconoBoton,
-                color = Color.Black,
-                fontSize = 40.sp
+            heroeItemButton(
+                expanded = expanded,
+                onClick = {expanded = !expanded}
             )
-            }
-
             iconoHeroe(imaxeId = heroe.imaxeId)
-
         }
         if (expanded) {
             extraHeroe(extra = heroe.extra)
         }
-
-
     }
 }
+
+@Composable
+fun heroeItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = if(expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
 
 @Composable
 fun extraHeroe(
@@ -174,8 +177,8 @@ fun extraHeroe(
             .fillMaxWidth()
             .padding(16.dp)
     ) {
-        Column (
-        ){
+        Column(
+        ) {
             Text(
                 text = "About:",
                 fontStyle = FontStyle.Normal,
