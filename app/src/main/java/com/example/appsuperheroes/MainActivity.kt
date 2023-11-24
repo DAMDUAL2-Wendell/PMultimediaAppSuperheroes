7cfe19f590ea2c0b9b99eda949487c80fc9378a3
wendellchabaud <wendelledgardossantoschabaud@gmail.com>
2023-11-24 13:05:09 +0100

    Finalizada App. Arregados tamanhos de imagenes. Agregada CenterAlignedTopAppBar. Arreglado cambio de color al pulsar el boton de visualizar info extra.

--- app/src/main/java/com/example/appsuperheroes/MainActivity.kt
+++ app/src/main/java/com/example/appsuperheroes/MainActivity.kt
@@ -11,9 +11,11 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -22,9 +24,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -37,17 +38,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.appsuperheroes.data.SuperHeroesRepository
 import com.example.appsuperheroes.model.Superheroe
@@ -74,24 +76,57 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppSuperheroes(modifier: Modifier = Modifier) {
     Scaffold(
-        modifier = modifier
+        topBar = { heroeTopAppBar(modifier = modifier) }
     ) { paddingValues ->
         contenido(paddingValues = paddingValues)
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun heroeTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = MaterialTheme.colorScheme.inversePrimary)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_heroe_logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.logo_size))
+                        .padding(dimensionResource(id = R.dimen.padding_small))
+                )
+                Text(
+                    text = stringResource(id = R.string.app_name),
+
+                    style = MaterialTheme.typography.displaySmall
+                )
+            }
+
+        }
+    )
+}
+
 @Composable
 fun contenido(
-    paddingValues: PaddingValues,
-    modifier: Modifier = Modifier
+    paddingValues: PaddingValues
 ) {
     LazyColumn(
-        modifier = modifier
-            .fillMaxSize()
+        contentPadding = paddingValues,
+        modifier = Modifier
+            .padding(top = dimensionResource(id = R.dimen.padding_small))
     ) {
         items(SuperHeroesRepository.superHeroesList) {
             SuperHeroeCard(
-                heroe = it
+                heroe = it,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
             )
         }
     }
@@ -106,45 +141,48 @@ fun SuperHeroeCard(
     var expanded by remember { mutableStateOf(false) }
 
     val cardBackgroundColor by animateColorAsState(
-        targetValue = if(expanded) MaterialTheme.colorScheme.tertiaryContainer else
+        targetValue = if (expanded) MaterialTheme.colorScheme.tertiaryContainer else
             MaterialTheme.colorScheme.primaryContainer, label = ""
     )
 
     Card(
         modifier = modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .animateContentSize(
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioLowBouncy,
-                    stiffness = Spring.StiffnessLow
-                )
-            )
-            .background(color = cardBackgroundColor)
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioLowBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+                .background(color = cardBackgroundColor)
+                .padding(dimensionResource(id = R.dimen.padding_small))
         ) {
-            Column(
+            Row(
                 modifier = Modifier
-                    .weight(1f)
-                    .padding(end = 16.dp)
+                    .fillMaxWidth()
             ) {
                 infoHeroe(
                     title = heroe.title,
-                    description = heroe.description
+                    description = heroe.description,
+                    modifier = Modifier.weight(0.6F)
+                )
+                //Spacer(modifier.weight(0.05f))
+                heroeItemButton(
+                    modifier = Modifier.weight(0.1f),
+                    expanded = expanded,
+                    onClick = { expanded = !expanded }
+                )
+
+                iconoHeroe(
+                    modifier = Modifier.weight(0.3f),
+                    imaxeId = heroe.imaxeId
                 )
             }
-            heroeItemButton(
-                expanded = expanded,
-                onClick = {expanded = !expanded}
-            )
-            iconoHeroe(imaxeId = heroe.imaxeId)
-        }
-        if (expanded) {
-            extraHeroe(extra = heroe.extra)
+            if (expanded) {
+                extraHeroe(extra = heroe.extra, modifier)
+            }
         }
     }
 }
@@ -160,7 +198,7 @@ fun heroeItemButton(
         modifier = modifier
     ) {
         Icon(
-            imageVector = if(expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+            imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
             contentDescription = null,
             tint = MaterialTheme.colorScheme.secondary
         )
@@ -170,25 +208,24 @@ fun heroeItemButton(
 
 @Composable
 fun extraHeroe(
-    @StringRes extra: Int
+    @StringRes extra: Int,
+    modifier: Modifier = Modifier
 ) {
-    Row(
-        modifier = Modifier
+    Column(
+        modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp)
     ) {
-        Column(
-        ) {
-            Text(
-                text = "About:",
-                fontStyle = FontStyle.Normal,
-                fontFamily = FontFamily.Default,
-                fontSize = 15.sp
-            )
-            Text(
-                text = stringResource(id = extra)
-            )
-        }
+
+        Text(
+            text = "About:",
+            fontStyle = FontStyle.Normal,
+            fontFamily = FontFamily.Default,
+            fontSize = 15.sp
+        )
+        Text(
+            text = stringResource(id = extra)
+        )
+
 
     }
 
@@ -200,12 +237,13 @@ fun iconoHeroe(
     modifier: Modifier = Modifier
 ) {
     Image(
-        painter = painterResource(id = imaxeId),
-        contentDescription = null,
         modifier = modifier
-            .size(80.dp, 80.dp)
-            .clip(MaterialTheme.shapes.medium),
-        contentScale = ContentScale.Crop
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .clip(MaterialTheme.shapes.small),
+        contentScale = ContentScale.Crop,
+        painter = painterResource(imaxeId),
+        contentDescription = null
     )
 }
 
@@ -218,6 +256,7 @@ fun infoHeroe(
 
     Column(
         modifier = modifier
+            .padding(dimensionResource(id = R.dimen.padding_small))
     ) {
         Text(
             text = stringResource(id = title),
